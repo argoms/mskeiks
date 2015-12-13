bfa832ee4e384c9ad41e309dd2c773ee3613c9b0
argoms <katana.argoms@gmail.com>
2015-12-12 21:13:41 -0800

    v0.0.6
    finished tut, time to actually make stuff

--- Assets/NetworkManager.cs
+++ Assets/NetworkManager.cs
@@ -1,4 +1,5 @@
-﻿using UnityEngine;
+﻿
+using UnityEngine;
 using System.Collections;
 
 public class NetworkManager :Photon. MonoBehaviour
@@ -6,7 +7,7 @@ public class NetworkManager :Photon. MonoBehaviour
   private const string roomName = "RoomName";
   private TypedLobby lobbyName = new TypedLobby("New_Lobby", LobbyType.Default);
   private RoomInfo[] roomsList;
-  public GameObject player;
+  public GameObject playerPrefab;
 
   void Start()
   {
@@ -67,62 +68,6 @@ public class NetworkManager :Photon. MonoBehaviour
   void OnJoinedRoom()
   {
     Debug.Log("Connected to Room");
-    PhotonNetwork.Instantiate(player.name, Vector3.up * 5, Quaternion.identity, 0);
+    GameObject newPlayer = PhotonNetwork.Instantiate(playerPrefab.name, Vector3.zero, Quaternion.identity, 0);
   }
-
-  /*
-  // Use this for initialization
-  void Start()
-  {
-    PhotonNetwork.ConnectUsingSettings("0.1");
-    Debug.Log("potato");
-    TextMesh myText = GetComponent<TextMesh>();
-    myText.text = "Aaa";
-  }
-  
-  private const string roomName = "RoomName";
-  private RoomInfo[] roomsList;
-
-  void OnGUI()
-  {
-    if (!PhotonNetwork.connected)
-    {
-      GUILayout.Label(PhotonNetwork.connectionStateDetailed.ToString());
-
-    }
-    else if (PhotonNetwork.room == null)
-    {
-      // Create Room
-      if (GUI.Button(new Rect(100, 100, 250, 100), "Start Server"))
-        //PhotonNetwork.CreateRoom(roomName + System.Guid.NewGuid().ToString("N"), true, true, 5);
-        PhotonNetwork.CreateRoom(roomName + System.Guid.NewGuid().ToString("N"));//, true, true, 5);
-      // Join Room
-      if (roomsList != null)
-      {
-        for (int i = 0; i < roomsList.Length; i++)
-        {
-          if (GUI.Button(new Rect(100, 250 + (110 * i), 250, 100), "Join " + roomsList[i].name))
-            PhotonNetwork.JoinRoom(roomsList[i].name);
-        }
-      }
-    }
-  }
-
-  void OnReceivedRoomListUpdate()
-  {
-    roomsList = PhotonNetwork.GetRoomList();
-  }
-  void OnJoinedRoom()
-  {
-    Debug.Log("Connected to Room");
-    Debug.Log("room: " + PhotonNetwork.room.name);
-  }
-
-  
-	// Update is called once per frame
-	void Update () {
-	  TextMesh myText = GetComponent<TextMesh>();
-    myText.text = "Aaa";
-    Debug.Log(R
-	}*/
 }
