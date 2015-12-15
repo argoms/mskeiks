3582e355870afe7c05c84295c6fec2ed983155ee
argoms <katana.argoms@gmail.com>
2015-12-15 15:28:54 -0800

    0.0.14
    -players look at mouse (synced between clients)
    -no interp/smoothing yet

--- Assets/PlayerControl.cs
+++ Assets/PlayerControl.cs
@@ -6,6 +6,7 @@ public class PlayerControl : Photon.MonoBehaviour
 
   public float speed = 10f;
   Rigidbody2D rigidbody;
+  Transform transform;
 
   private float lastSynchronizationTime = 0f;
   private float syncDelay = 0f;
@@ -17,21 +18,28 @@ public class PlayerControl : Photon.MonoBehaviour
   private bool attacking;
 
   public bool canMove;
+
+  private Quaternion rotation;
   void Start()
   {
     canMove = true;
     attacking = false;
     rigidbody = GetComponent<Rigidbody2D>();
+    transform = GetComponent<Transform>();
     camera = GameObject.Find("Camera");
   }
   void Update()
   {
+    
+
     //Debug.Log(PhotonNetwork.GetPing());
     if (photonView.isMine)
     {
 
       camera.transform.position = GetComponent<Transform>().position + new Vector3(0, -2, -12); //camera follows player default -12, -30 for more zoom
       InputMovement();
+
+      
     }
     else
     {
@@ -43,6 +51,12 @@ public class PlayerControl : Photon.MonoBehaviour
 
   void InputMovement()
   {
+    Vector3 mousePos = Input.mousePosition;
+    mousePos.z = 12;
+    mousePos = Camera.main.ScreenToWorldPoint(mousePos);
+    transform.rotation = Quaternion.LookRotation(Vector3.forward, mousePos - transform.position);
+    Debug.Log(Input.mousePosition);
+
     Vector2 movement = new Vector2(0,0);
     if (Input.GetKey(KeyCode.W))
     {
@@ -96,6 +110,8 @@ public class PlayerControl : Photon.MonoBehaviour
 
     syncTime += Time.deltaTime;
     rigidbody.position = Vector2.Lerp(syncStartPosition, syncEndPosition, syncTime / syncDelay);
+    transform.rotation = rotation;
+    
   }
 
   void OnPhotonSerializeView(PhotonStream stream, PhotonMessageInfo info)
@@ -103,6 +119,7 @@ public class PlayerControl : Photon.MonoBehaviour
     if (stream.isWriting)
     {
       stream.SendNext(rigidbody.position);
+      stream.SendNext(transform.rotation);
       //stream.SendNext(rigidbody.velocity);
     }
     else
@@ -111,7 +128,7 @@ public class PlayerControl : Photon.MonoBehaviour
       
       syncEndPosition = (Vector2)stream.ReceiveNext();
       syncStartPosition = rigidbody.position;
-
+      rotation = (Quaternion)stream.ReceiveNext();
       //attacking = (bool)stream.ReceiveNext();
       syncTime = 0f;
       syncDelay = Time.time - lastSynchronizationTime;
